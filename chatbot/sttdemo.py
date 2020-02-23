28e686413190135f23ca51e4f2dac065dbd82438
ren shan <ren.shan@unisys.com>
2020-02-22 20:38:44 -0500

    merging

--- chatbot/sttdemo.py
+++ chatbot/sttdemo.py
@@ -1,11 +1,3 @@
-from flask import Flask, render_template, request, redirect
-from nltk import word_tokenize
-from nltk.stem.lancaster import LancasterStemmer
-from numpy import array, argmax
-from keras import models
-from pickle import load
-from random import choice
-
 from gtts import gTTS
 import speech_recognition as sr
 import os
@@ -13,11 +5,12 @@ import re
 import webbrowser
 import smtplib
 import requests
+#from weather import Weather
 
-audiosw = True
 def talkToMe(audio):
     "speaks audio passed as argument"
 
+    print(audio)
     for line in audio.splitlines():
         os.system("say " + audio)
 
@@ -136,56 +129,9 @@ def assistant(command):
     #             talkToMe('On %s will it %s. The maximum temperture will be %.1f degree.'
     #                      'The lowest temperature will be %.1f degrees.' % (forecasts[i].date(), forecasts[i].text(), (int(forecasts[i].high())-32)/1.8, (int(forecasts[i].low())-32)/1.8))
     
-if (audiosw): 
-    talkToMe('I am ready for your command')
-    #loop to continue executing multiple commands
-    while True:
-        assistant(myCommand())
+        
+talkToMe('I am ready for your command')
 
-
-def getQueryFeatures(query):
-    queryTokens = word_tokenize(query)
-    queryStems = sorted(list(set([stemmer.stem(w.lower()) for w in queryTokens if w not in ignored])))
-    queryBag = []
-    for w in vocabulary:
-        queryBag.append(1) if w in queryStems else queryBag.append(0)
-    queryBag = array(queryBag)
-    return queryBag.reshape(1, len(vocabulary))
-
-with open('trainedModel/vars.pkl', 'rb') as f:
-    vocabulary, classes, ignored, intents = load(f)
-intentsDict = {i['tag']: i['responses'] for i in intents['intents']}
-model = models.load_model('trainedModel/FAQbot_model.h5')
-stemmer = LancasterStemmer()
-
-def get_response(query):
-    queryBag = getQueryFeatures(query)
-    model.predict(queryBag)
-    idx = argmax(model.predict(queryBag))
-    return choice(intentsDict[classes[idx]])
-
-query = "This is me"
-reply = "Hello there"
-
-app = Flask(__name__)
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template('index.html', questionAsked=query, response=reply,audioSwitch = audiosw)
-
-@app.route('/signup', methods = ['POST'])
-def signup():
-	global query
-	global reply
-	query = request.form['question']
-	response = get_response(query)
-	print(response)
-	reply = response
-	return redirect('/')
-
-def triggerAudio():
-    audiosw = False if audiosw else True
-    print('audiosw:',audiosw)   
-
-if __name__ == "__main__":
-    app.run()
+#loop to continue executing multiple commands
+while True:
+    assistant(myCommand())
